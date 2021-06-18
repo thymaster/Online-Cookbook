d8df06ded6f02a2dcd8c95851c7368121134dd1f
Anish Sarkar <anish_pg@ece.nits.ac.in>
2021-06-18 16:21:47 +0530

    button hover transition

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,29 +3,31 @@ import "./Navbar.css";
 
 export const Navbar = () => {
   return (
-    <section className="navbar">
-      <div className="logo">
-        <span>
-          <h1>Bits n Bytes</h1>
-        </span>
-      </div>
-      <div className="signUp">
-        <span>
-          <button className="btn btn-slide">
-            <h2>Contact</h2>
-          </button>
-        </span>
-        <span>
-          <button className="btn btn-slide">
-            <h2>Sign In</h2>
-          </button>
-        </span>
-        <span>
-          <button className="btn btn-slide">
-            <h2>Sign Up</h2>
-          </button>
-        </span>
-      </div>
-    </section>
+    <body>
+      <header className="navbar">
+        <div className="logo">
+          <span>
+            <h1>Bits n Bytes</h1>
+          </span>
+        </div>
+        <div className="signup">
+          <span>
+            <button className="btn btn-slide">
+              <h2>Contact</h2>
+            </button>
+          </span>
+          <span>
+            <button className="btn btn-slide">
+              <h2>Sign In</h2>
+            </button>
+          </span>
+          <span>
+            <button className="btn btn-slide">
+              <h2>Sign Up</h2>
+            </button>
+          </span>
+        </div>
+      </header>
+    </body>
   );
 };
