1298d1c2ee45e36fcd2333bd71614f4d381749c1
Anish Sarkar <anish_pg@ece.nits.ac.in>
2021-06-20 01:48:24 +0530

    tailwind footer update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Footer.css";
+// import "./Footer.css";
 
 export const Footer = () => {
   return (
     <div>
-      <footer className="footer">
+      {/* <footer className="footer">
         <div className="brand-title-footer">
           <h4>
             <a href="#">Bits n Bytes</a>
@@ -40,6 +40,108 @@ export const Footer = () => {
           </ul>
         </div>
         <div>C 2021 Bits n Bytes, ALL RIGHTS RESERVED</div>
+      </footer> */}
+      <footer class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+            <a href="https://www.google.com" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+              <svg
+                // xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                // class="w-10 h-10 text-white p-2 bg-red-500 rounded-full"
+                viewBox="0 0 122.88 120.4"
+                id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" 
+              >
+                <path class="cls-1" d="M3,103.28q33.87-.07,67.73,0a2.46,2.46,0,0,1,2.45,2.44c0,3.65-1.54,10.79-9.79,10.79H10.35c-8.25,0-9.78-7.14-9.78-10.79A2.45,2.45,0,0,1,3,103.28Zm56.86-59c18.83,14.5,25.55,8.78,36.34,3.46,8.27-4.07,11.5-7.93,21.54-3.86V29.1H59.88V44.28ZM96.62,24.65l3.78-14a3.42,3.42,0,0,1,1.82-2.3L117.7.44a3.48,3.48,0,0,1,3.4,6.07l-14.28,7.26-3,10.88.27-.84h16.5A2.22,2.22,0,0,1,122.77,26h0V54.26c0,18.35-10,32.59-28.74,34v25.82h14.55c1.23,0,2.21,1.94,2.21,3.16s-1,3.17-2.21,3.17H76.82l.48-.51a19.07,19.07,0,0,0,3.57-5.82h3.78V88.34c-1,0-2-.12-3-.23a15,15,0,0,0-1.06-2.57,13.5,13.5,0,0,0,1.26-1.89,14.16,14.16,0,0,0,1-11.25,23.44,23.44,0,0,0-4.81-8.35c-4.58-5.28-12.65-10.66-23.47-13.6V26a2.21,2.21,0,0,1,2.21-2.21H96.87l-.25.84ZM67.55,81.78H7a7,7,0,0,1-7-7H0c0-10,19.85-17.21,36.53-17.37,32.8-.33,47.52,24.36,31,24.36Zm-14-16.14a3.77,3.77,0,1,1-3.76,3.77,3.77,3.77,0,0,1,3.76-3.77Zm-32.81.27A3.77,3.77,0,1,1,17,69.68a3.77,3.77,0,0,1,3.76-3.77Zm16.28-3.59a3.77,3.77,0,1,1-3.77,3.77,3.77,3.77,0,0,1,3.77-3.77ZM73,91.38a1.13,1.13,0,0,1,.16.56c0,.13,0,.27,0,.41s0,.28,0,.4a1.17,1.17,0,0,1-.16.57c-.55,2.42-2.78,5.6-9.62,5.6H10.35c-6.84,0-9.06-3.17-9.62-5.6a1.17,1.17,0,0,1-.16-.57c0-.12,0-.26,0-.4a2.88,2.88,0,0,1,0-.41,1.13,1.13,0,0,1,.16-.56c.56-2.43,2.79-5.6,9.62-5.6H37.83L48,96.24,58,85.78h5.39c6.84,0,9.07,3.17,9.62,5.6Z" />
+              </svg>
+              <span class="ml-3 text-xl">Bits n Bytes</span>
+            </a>
+            <p class="mt-2 text-sm text-gray-500">
+              Cook anything you imagine  
+            </p>
+          </div>
+          <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                <a href="https://www.google.com" class="text-gray-600 hover:text-gray-800">ABOUT US</a> 
+              </h2>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                <a href="https://www.google.com" class="text-gray-600 hover:text-gray-800">PRIVACY POLICY</a> 
+              </h2>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                <a href="https://www.google.com" class="text-gray-600 hover:text-gray-800">TERMS AND CONDITIONS</a> 
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div class="bg-gray-100">
+          <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p class="text-gray-500 text-sm text-center sm:text-left">
+              © 2021 Bits n Bytes —
+              <a
+                href="https://github.com/thymaster"
+                rel="noopener noreferrer"
+                class="text-gray-600 ml-1"
+                target="_blank"
+              >
+                thymaster
+              </a>
+            </p>
+            <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+              <a  href="https://twitter.com/Anish16065" class="ml-3 text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a href="https://www.instagram.com/commandercoriandersalamander/" class="ml-3 text-gray-500">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a href="https://www.linkedin.com/in/anish-sarkar-6a3282152/" class="ml-3 text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="0"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  ></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
       </footer>
     </div>
   );
