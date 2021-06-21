e962ff8b1e2d74025e9468027f4e53725aaa23ec
Anish Sarkar <anish_pg@ece.nits.ac.in>
2021-06-21 18:05:38 +0530

    Name added as h1 is contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,23 +6,37 @@ import "../Styles/Contact.css";
 export const Contact = () => {
   return (
     <div>
-      <Navbar />
+      <Navbar className="navbar-style-for-contact" />
       {/* <Link to="/contact"></Link> */}
       <div className="floating-container">
+        <h1 className="designer-name">Anish Sarkar</h1>
         <div className="floating-card">
           <div className="information">
             <div className="photopraph-div">
               <div className="photograph-alignment">
                 <div className="photograph">
-                  <img className="picture" src="./anish_400x400.jpg" alt="avatar" />
+                  <img
+                    className="picture"
+                    src="./anish_400x400.jpg"
+                    alt="avatar"
+                  />
                 </div>
               </div>
             </div>
             <p className="about">
-              As the designer and developer I take care of the visual features and the technical details to make sure you have seamless
-              experience. Should you come across any bug, feel free to contact me.
+              As the designer and developer I take care of the visual features
+              and the technical details to make sure you have seamless
+              experience. Should you come across any bug, feel free to contact
+              me.
             </p>
-            <address className="email"><a className="email-hyperlink" href="mailto:sarkaranish0099@gmail.com">sarkaranish0099@gmail.com</a></address>
+            <address className="email">
+              <a
+                className="email-hyperlink"
+                href="mailto:sarkaranish0099@gmail.com"
+              >
+                sarkaranish0099@gmail.com
+              </a>
+            </address>
           </div>
           <div className="graphic"></div>
         </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Styles/Footer.css";
 
 export const Footer = () => {
@@ -45,44 +46,76 @@ export const Footer = () => {
       <footer className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-            <a href="https://www.google.com" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <svg
-                fill="green"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-20 h-20 rounded-half"
-                viewBox="0 0 122.88 120.4"
-                id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" 
+            <Link to="/">
+              <a
+                href="/"
+                className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
               >
-                <path className="cls-1" d="M3,103.28q33.87-.07,67.73,0a2.46,2.46,0,0,1,2.45,2.44c0,3.65-1.54,10.79-9.79,10.79H10.35c-8.25,0-9.78-7.14-9.78-10.79A2.45,2.45,0,0,1,3,103.28Zm56.86-59c18.83,14.5,25.55,8.78,36.34,3.46,8.27-4.07,11.5-7.93,21.54-3.86V29.1H59.88V44.28ZM96.62,24.65l3.78-14a3.42,3.42,0,0,1,1.82-2.3L117.7.44a3.48,3.48,0,0,1,3.4,6.07l-14.28,7.26-3,10.88.27-.84h16.5A2.22,2.22,0,0,1,122.77,26h0V54.26c0,18.35-10,32.59-28.74,34v25.82h14.55c1.23,0,2.21,1.94,2.21,3.16s-1,3.17-2.21,3.17H76.82l.48-.51a19.07,19.07,0,0,0,3.57-5.82h3.78V88.34c-1,0-2-.12-3-.23a15,15,0,0,0-1.06-2.57,13.5,13.5,0,0,0,1.26-1.89,14.16,14.16,0,0,0,1-11.25,23.44,23.44,0,0,0-4.81-8.35c-4.58-5.28-12.65-10.66-23.47-13.6V26a2.21,2.21,0,0,1,2.21-2.21H96.87l-.25.84ZM67.55,81.78H7a7,7,0,0,1-7-7H0c0-10,19.85-17.21,36.53-17.37,32.8-.33,47.52,24.36,31,24.36Zm-14-16.14a3.77,3.77,0,1,1-3.76,3.77,3.77,3.77,0,0,1,3.76-3.77Zm-32.81.27A3.77,3.77,0,1,1,17,69.68a3.77,3.77,0,0,1,3.76-3.77Zm16.28-3.59a3.77,3.77,0,1,1-3.77,3.77,3.77,3.77,0,0,1,3.77-3.77ZM73,91.38a1.13,1.13,0,0,1,.16.56c0,.13,0,.27,0,.41s0,.28,0,.4a1.17,1.17,0,0,1-.16.57c-.55,2.42-2.78,5.6-9.62,5.6H10.35c-6.84,0-9.06-3.17-9.62-5.6a1.17,1.17,0,0,1-.16-.57c0-.12,0-.26,0-.4a2.88,2.88,0,0,1,0-.41,1.13,1.13,0,0,1,.16-.56c.56-2.43,2.79-5.6,9.62-5.6H37.83L48,96.24,58,85.78h5.39c6.84,0,9.07,3.17,9.62,5.6Z" />
-              </svg>
-              <span className="ml-3 text-xl">Bits n Bytes</span>
-            </a>
+                <svg
+                  fill="green"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-20 h-20 rounded-half"
+                  viewBox="0 0 122.88 120.4"
+                  id="Layer_1"
+                  data-name="Layer 1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    className="cls-1"
+                    d="M3,103.28q33.87-.07,67.73,0a2.46,2.46,0,0,1,2.45,2.44c0,3.65-1.54,10.79-9.79,10.79H10.35c-8.25,0-9.78-7.14-9.78-10.79A2.45,2.45,0,0,1,3,103.28Zm56.86-59c18.83,14.5,25.55,8.78,36.34,3.46,8.27-4.07,11.5-7.93,21.54-3.86V29.1H59.88V44.28ZM96.62,24.65l3.78-14a3.42,3.42,0,0,1,1.82-2.3L117.7.44a3.48,3.48,0,0,1,3.4,6.07l-14.28,7.26-3,10.88.27-.84h16.5A2.22,2.22,0,0,1,122.77,26h0V54.26c0,18.35-10,32.59-28.74,34v25.82h14.55c1.23,0,2.21,1.94,2.21,3.16s-1,3.17-2.21,3.17H76.82l.48-.51a19.07,19.07,0,0,0,3.57-5.82h3.78V88.34c-1,0-2-.12-3-.23a15,15,0,0,0-1.06-2.57,13.5,13.5,0,0,0,1.26-1.89,14.16,14.16,0,0,0,1-11.25,23.44,23.44,0,0,0-4.81-8.35c-4.58-5.28-12.65-10.66-23.47-13.6V26a2.21,2.21,0,0,1,2.21-2.21H96.87l-.25.84ZM67.55,81.78H7a7,7,0,0,1-7-7H0c0-10,19.85-17.21,36.53-17.37,32.8-.33,47.52,24.36,31,24.36Zm-14-16.14a3.77,3.77,0,1,1-3.76,3.77,3.77,3.77,0,0,1,3.76-3.77Zm-32.81.27A3.77,3.77,0,1,1,17,69.68a3.77,3.77,0,0,1,3.76-3.77Zm16.28-3.59a3.77,3.77,0,1,1-3.77,3.77,3.77,3.77,0,0,1,3.77-3.77ZM73,91.38a1.13,1.13,0,0,1,.16.56c0,.13,0,.27,0,.41s0,.28,0,.4a1.17,1.17,0,0,1-.16.57c-.55,2.42-2.78,5.6-9.62,5.6H10.35c-6.84,0-9.06-3.17-9.62-5.6a1.17,1.17,0,0,1-.16-.57c0-.12,0-.26,0-.4a2.88,2.88,0,0,1,0-.41,1.13,1.13,0,0,1,.16-.56c.56-2.43,2.79-5.6,9.62-5.6H37.83L48,96.24,58,85.78h5.39c6.84,0,9.07,3.17,9.62,5.6Z"
+                  />
+                </svg>
+                <span className="ml-3 text-xl">Bits n Bytes</span>
+              </a>
+            </Link>
             <p className="mt-2 text-sm text-gray-500">
-              Cook anything you imagine  
+              Cook anything you imagine
             </p>
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                <a href="https://www.google.com" className="text-gray-600 hover:text-gray-800">ABOUT US</a> 
+                <a
+                  href="https://www.google.com"
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  ABOUT US
+                </a>
               </h2>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                <a href="https://www.google.com" className="text-gray-600 hover:text-gray-800">PRIVACY POLICY</a> 
+                <a
+                  href="https://www.google.com"
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  PRIVACY POLICY
+                </a>
               </h2>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                <a href="https://www.google.com" className="text-gray-600 hover:text-gray-800">CONTACT US</a> 
+                <Link to="/contact">
+                  <a
+                    href="/contact"
+                    className="text-gray-600 hover:text-gray-800"
+                  >
+                    CONTACT US
+                  </a>
+                </Link>
               </h2>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                <a href="https://www.google.com" className="text-gray-600 hover:text-gray-800">TERMS AND CONDITIONS</a> 
+                <a
+                  href="https://www.google.com"
+                  className="text-gray-600 hover:text-gray-800"
+                >
+                  TERMS AND CONDITIONS
+                </a>
               </h2>
             </div>
           </div>
@@ -101,7 +134,10 @@ export const Footer = () => {
               </a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <a  href="https://twitter.com/Anish16065" className="ml-3 text-gray-500">
+              <a
+                href="https://twitter.com/Anish16065"
+                className="ml-3 text-gray-500"
+              >
                 <svg
                   fill="currentColor"
                   stroke-linecap="round"
@@ -113,7 +149,10 @@ export const Footer = () => {
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
-              <a href="https://www.instagram.com/commandercoriandersalamander/" className="ml-3 text-gray-500">
+              <a
+                href="https://www.instagram.com/commandercoriandersalamander/"
+                className="ml-3 text-gray-500"
+              >
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -127,7 +166,10 @@ export const Footer = () => {
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </a>
-              <a href="https://www.linkedin.com/in/anish-sarkar-6a3282152/" className="ml-3 text-gray-500">
+              <a
+                href="https://www.linkedin.com/in/anish-sarkar-6a3282152/"
+                className="ml-3 text-gray-500"
+              >
                 <svg
                   fill="currentColor"
                   stroke="currentColor"
