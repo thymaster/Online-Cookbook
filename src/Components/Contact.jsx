43e3f6704aca7e50338b13976df46eb47ab9aa08
Anish Sarkar <anish_pg@ece.nits.ac.in>
2021-06-21 17:22:36 +0530

    reference by element removed, used classname for repeating elements, Contact page done (image visibilty issue)

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -14,10 +14,15 @@ export const Contact = () => {
             <div className="photopraph-div">
               <div className="photograph-alignment">
                 <div className="photograph">
-                  <img src="anish_400x400.jpg" alt="avatar" />
+                  <img className="picture" src="./anish_400x400.jpg" alt="avatar" />
                 </div>
               </div>
             </div>
+            <p className="about">
+              As the designer and developer I take care of the visual features and the technical details to make sure you have seamless
+              experience. Should you come across any bug, feel free to contact me.
+            </p>
+            <address className="email"><a className="email-hyperlink" href="mailto:sarkaranish0099@gmail.com">sarkaranish0099@gmail.com</a></address>
           </div>
           <div className="graphic"></div>
         </div>
